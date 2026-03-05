023660471e7fc6f152c8688f7ddf08123d4257c7
lucasbe-lpr <lucas.bessonnat@leprogres.fr>
2026-03-05 11:22:19 +0100

    v1.0

--- app.py
+++ app.py
@@ -717,6 +717,6 @@ with tab_t:
 st.markdown("""
 <div class="site-footer">
   <span class="footer-name">© lucas bessonnat</span>
-  <span>Aucune donnée n'est conservée sur un serveur.</span>
+  <span>v1.0. Aucune donnée n'est conservée sur un serveur.</span>
 </div>
 """, unsafe_allow_html=True)
